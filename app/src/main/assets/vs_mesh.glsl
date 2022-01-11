5c04b026ec9462ad2bb34324c23d4afb0a6871ff
MarcoCiaramella <marcociaramella87@gmail.com>
2022-01-11 17:33:47 +0100

    refactoring

--- app/src/main/assets/vs_mesh.glsl
+++ app/src/main/assets/vs_mesh.glsl
@@ -3,19 +3,20 @@
 precision highp float;
 
 
+const int MAX_JOINTS = 60;
 uniform mat4 uMVPMatrix;
 uniform mat4 uMMatrix;
 attribute vec4 aPosition;
 attribute vec3 aNormal;
 attribute vec2 aTexCoords;
-varying vec4 vPosition;
-varying vec3 vNormal;
-varying vec2 vTexCoords;
-const int MAX_JOINTS = 60;
 attribute vec3 aJointIndices;
 attribute vec3 aWeights;
 uniform mat4 uBindShapeMatrix;
 uniform mat4 uJointTransforms[MAX_JOINTS];
+varying vec4 vPosition;
+varying vec3 vNormal;
+varying vec2 vTexCoords;
+
 
 
 
